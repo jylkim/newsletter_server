3ff12c5520f0529d632659c1b9464edca3f6b4ae
jylkim <jameskim527@gmail.com>
2024-05-06 11:04:26 +0900

    Add health check, subscriptions endpoint Add database connection Add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use newsletter_server::configuration::get_configuration;
+use newsletter_server::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
